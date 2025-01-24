d4b7f28263073713584b0b83afee591ae16e8b92
LuYixian <mcluoc@gmail.com>
2025-01-24 12:30:24 +0900

    update: conda setting

---
+++ home/dot_custom/alias.sh
@@ -0,0 +1,21 @@
+# factory functions
+function alias_if_cmd_exists() {
+    cmd=$1
+    alias_name=$2
+    if type $cmd >/dev/null 2>&1; then
+        alias $alias_name=$cmd
+    fi
+}
+
+alias_if_cmd_exists "chezmoi" "dot"
+alias_if_cmd_exists "nvim" "vi"
+alias_if_cmd_exists "nvim" "vim"
+alias_if_cmd_exists "nvim" "view"
+alias_if_cmd_exists "lsd" "ls"
+alias ll="ls -l"
+alias la="ls -a"
+alias lla="ls -la"
+alias_if_cmd_exists "bat" "cat"
+alias_if_cmd_exists "dust" "du"
+alias_if_cmd_exists "tldr" "man"
+alias_if_cmd_exists "hyperfine" "hf"
---
+++ home/dot_custom/eval.sh
@@ -0,0 +1,13 @@
+# factory functions
+function eval_if_cmd_exists() {
+    cmd=$1
+    eval_tgt=$2
+    if type $cmd >/dev/null 2>&1; then
+        eval "$(eval $eval_tgt)"
+    fi
+}
+
+eval_if_cmd_exists "starship" "starship init zsh"
+eval_if_cmd_exists "sheldon" "sheldon source"
+eval_if_cmd_exists "fzf" "fzf --zsh"
+eval_if_cmd_exists "navi" "navi widget zsh"
