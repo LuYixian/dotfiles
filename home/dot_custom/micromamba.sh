75ac4839bed767e5cfc5ec12ff6854a588c2eac2
LuYixian <mcluoc@gmail.com>
2025-01-27 20:48:27 +0800

    update: lazygit config

--- home/dot_custom/micromamba.sh
+++
@@ -1,7 +0,0 @@
-__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__mamba_setup"
-else
-    alias micromamba="$MAMBA_EXE"  # Fallback on help from micromamba activate
-fi
-unset __mamba_setup
