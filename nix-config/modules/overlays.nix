b356dfce418f659c87b8137c06278e958c074d9c
LuYixian <mcluoc@gmail.com>
2025-11-19 22:34:12 +0900

    rm: unnecessary config

--- nix-config/modules/overlays.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: {
-  nixpkgs.overlays = [
-    (self: super: {
-      azure-cli = super.azure-cli.overrideAttrs (oldAttrs: {
-        buildInputs = (oldAttrs.buildInputs or []) ++ [ (super.python3.withPackages (ps: [ ps.pip ])) ];
-      });
-    })
-  ];
-} 
