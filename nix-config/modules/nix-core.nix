423499978f806100141f2162e20d6059c543e8d7
LuYixian <mcluoc@gmail.com>
2025-12-31 01:36:21 +0900

    refactor: modernize tooling and simplify nix-darwin config
      - Replace lsd with eza for better git integration in aliases
      - Add devenv, cachix, python3, duf to home packages
      - Simplify nix-core.nix by removing unused params (Determinate Nix)
      - Simplify configurationRevision using self.rev instead of bash script
      - Add architecture detection for Paperlib (arm64/x64)
      - Update flake.lock dependencies
    
      Or a shorter version:
    
      update: switch to eza, add devenv, simplify nix-darwin config

--- nix-config/modules/nix-core.nix
+++ nix-config/modules/nix-core.nix
@@ -1,25 +1,7 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
-  # Enable alternative shell support in nix-darwin.
-  # programs.fish.enable = true;
-
+{...}: {
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Determinate Nix is installed; avoid conflicting daemon/config management.
   nix.enable = false;
-
-  nix.package = pkgs.nix;
-
-  nix.settings = {
-    # Necessary for using flakes on this system.
-    experimental-features = "nix-command flakes";
-    max-jobs = 8;
-    # Disable auto-optimise-store because of this issue:
-    #   https://github.com/NixOS/nix/issues/7273
-    # "error: cannot link '/nix/store/.tmp-link-xxxxx-xxxxx' to '/nix/store/.links/xxxx': File exists"
-    auto-optimise-store = false;
-  };
 }
